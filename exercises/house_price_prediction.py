fa34ef8d58e2805bc54a0cb6b5ddd5765c757d0b
Daniel Winter <daniwin@cs.huji.ac.il>
2022-04-06 19:52:05 +0300

    started the polynomial regressor

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
     X, y = load_data("../datasets/house_prices.csv")
 
     # Question 2 - Feature evaluation with respect to response
-    #feature_evaluation(X, y, r".")
+    feature_evaluation(X, y, r".")
 
     # Question 3 - Split samples into training- and testing sets.
     train_X, train_y, test_X, test_y = split_train_test(X, y, 0.75)
